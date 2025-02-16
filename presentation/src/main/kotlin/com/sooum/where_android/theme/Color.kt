4db8948aa1e0e8affd0d17e0114962023f1f66e7
jmseb3 <jmseb3@naver.com>
2025-02-16 20:20:00 +0900

    feat: UserItemView 추가
    - UserViewType에 따른 UserItemView 대응

---
+++ presentation/src/main/kotlin/com/sooum/where_android/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.sooum.where_android.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Gray50 = Color(0xffF9FAFB)
+val Gray100 = Color(0xffF3F4F6)
+val Gray200 = Color(0xffE5E7EB)
+val Gray300 = Color(0xffD1D5DB)
+val Gray400 = Color(0xff9CA3AF)
+val Gray500 = Color(0xff6B7280)
+val Gray600 = Color(0xff4B5563)
+val Gray700 = Color(0xff374151)
+val Gray800 = Color(0xff1F2937)
+val Gray900 = Color(0xff111827)
+
+
+val GrayScale100 = Color(0xffF1F3F5)
+val GrayScale300 = Color(0xffDEE2E6)
+
+
+val Primary600 = Color(0xff4f46E5)
