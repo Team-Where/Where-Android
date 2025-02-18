52042cad8824f4b8b7f13c99f4025474796025ee
park-hwaan <parkhwan1129@gmail.com>
2025-02-18 14:54:01 +0900

    feat: 뒤로가기,건너뛰기 클릭 이벤트 적용

--- presentation/src/main/kotlin/com/sooum/where_android/view/onboarding/OnBoardingActivity.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/onboarding/OnBoardingActivity.kt
@@ -2,7 +2,10 @@ package com.sooum.where_android.view.onboarding
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.sooum.where_android.view.MainActivity
 import com.sooum.where_android.databinding.ActivityOnBoardingBinding
 
@@ -16,9 +19,36 @@ class OnBoardingActivity : AppCompatActivity(){
 
         binding.container.adapter = ViewPagerAdapter(this)
 
+        binding.container.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == binding.container.adapter!!.itemCount -1) {
+                    binding.skipText.visibility = View.GONE
+                    binding.nextBtn.visibility = View.VISIBLE
+                } else {
+                    binding.skipText.visibility = View.VISIBLE
+                    binding.nextBtn.visibility = View.INVISIBLE
+                }
+            }
+        })
+
         binding.nextBtn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.skipText.setOnClickListener {
+            val nextItem = binding.container.currentItem + 1
+            if (nextItem < binding.container.adapter!!.itemCount) {
+                binding.container.currentItem = nextItem
+            }
+        }
+
+        binding.backImage.setOnClickListener {
+            val prevItem = binding.container.currentItem - 1
+            if (prevItem >= 0) {
+                binding.container.currentItem = prevItem
+            }
         }
     }
 }
--- presentation/src/main/kotlin/com/sooum/where_android/view/onboarding/ViewPagerAdapter.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/onboarding/ViewPagerAdapter.kt
@@ -5,30 +5,20 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ViewPagerAdapter(
-    private val fragmentActivity: FragmentActivity,
-    private val fragments : List<Fragment> = listOf(
+    fragmentActivity: FragmentActivity
+) : FragmentStateAdapter(fragmentActivity) {
+
+    private val fragments: List<Fragment> = listOf(
         OnBoardingFirstFragment(),
         OnBoardingSecondFragment(),
         OnBoardingThirdFragment()
     )
-) : FragmentStateAdapter(fragmentActivity)  {
-    private val itemCount = Int.MAX_VALUE
 
     override fun getItemCount(): Int {
-        return itemCount
+        return fragments.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        val realPosition = position % getRealItemCount()
-        return fragments[realPosition]
-    }
-
-    override fun getItemId(position: Int): Long {
-        val realPosition = position % getRealItemCount()
-        return fragments[realPosition].hashCode().toLong()
-    }
-
-    private fun getRealItemCount(): Int {
-        return fragments.size
+        return fragments[position]
     }
 }
