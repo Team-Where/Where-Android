7cdc0a4bcf4b5ee1415120338434c16b52b6435e
jmseb3 <jmseb3@naver.com>
2025-02-21 22:41:09 +0900

    feat: SingletonImageLoader 설정
    - https://coil-kt.github.io/coil/image_loaders/

--- presentation/src/main/kotlin/com/sooum/where_android/WhereApp.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/WhereApp.kt
@@ -1,9 +1,36 @@
 package com.sooum.where_android
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.memory.MemoryCache
+import coil3.network.okhttp.OkHttpNetworkFetcherFactory
+import coil3.request.crossfade
 import dagger.hilt.android.HiltAndroidApp
+import okhttp3.OkHttpClient
 
 @HiltAndroidApp
-class WhereApp : Application() {
+class WhereApp : Application(), SingletonImageLoader.Factory {
 
+    override fun newImageLoader(context: PlatformContext): ImageLoader {
+
+        return ImageLoader.Builder(context)
+            .components {
+                add(
+                    OkHttpNetworkFetcherFactory(
+                        callFactory = {
+                            OkHttpClient()
+                        }
+                    )
+                )
+            }
+            .memoryCache {
+                MemoryCache.Builder()
+                    .maxSizePercent(context, 0.25)
+                    .build()
+            }
+            .crossfade(true)
+            .build()
+    }
 }
