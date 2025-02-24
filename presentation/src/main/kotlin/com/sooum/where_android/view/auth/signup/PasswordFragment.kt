802f3452307cad599f2a80aafacdf2423fb17865
park-hwaan <parkhwan1129@gmail.com>
2025-02-24 14:43:19 +0900

    design: 비밀번호 설정화면 디자인

--- presentation/src/main/kotlin/com/sooum/where_android/view/auth/signup/PasswordFragment.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/auth/signup/PasswordFragment.kt
@@ -6,20 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sooum.where_android.databinding.FragmentEmailVerificationBinding
+import com.sooum.where_android.databinding.FragmentPasswordBinding
 import com.sooum.where_android.view.auth.AuthActivity
 
-class EmailVerificationFragment : Fragment() {
-    private lateinit var binding : FragmentEmailVerificationBinding
+class PasswordFragment : Fragment() {
+    private lateinit var binding : FragmentPasswordBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentEmailVerificationBinding.inflate(inflater, container, false)
-
-        binding.nextBtn.setOnClickListener {
-            (activity as AuthActivity).navigateToFragment(PasswordFragment())
-        }
+        binding = FragmentPasswordBinding.inflate(inflater, container, false)
 
         return binding.root
     }
