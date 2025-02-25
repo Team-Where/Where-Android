ea24cdd8d1a251db6698f515162208f3371dad9a
park-hwaan <parkhwan1129@gmail.com>
2025-02-25 14:00:55 +0900

    design: 프로필설정,회원가입완료 화면 디자인

--- presentation/src/main/kotlin/com/sooum/where_android/view/auth/signup/ProfileSettingFragment.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/auth/signup/ProfileSettingFragment.kt
@@ -6,19 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sooum.where_android.databinding.FragmentPasswordBinding
+import com.sooum.where_android.databinding.FragmentProfileSettingBinding
 import com.sooum.where_android.view.auth.AuthActivity
 
-class PasswordFragment : Fragment() {
-    private lateinit var binding : FragmentPasswordBinding
+class ProfileSettingFragment : Fragment() {
+    private lateinit var binding : FragmentProfileSettingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentPasswordBinding.inflate(inflater, container, false)
+        binding = FragmentProfileSettingBinding.inflate(inflater, container, false)
 
         binding.nextBtn.setOnClickListener {
-            (activity as AuthActivity).navigateToFragment(ProfileSettingFragment())
+            (activity as AuthActivity).navigateToFragment(SignUpCompleteFragment())
         }
 
         return binding.root
