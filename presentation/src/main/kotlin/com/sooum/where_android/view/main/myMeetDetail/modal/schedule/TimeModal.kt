b3c1d0705eb0336c3222eb58c55b99b8335ec17c
Wonddak <jmseb3@naver.com>
2025-03-21 10:24:27 +0900

    fix: 시간 선택 드롭다운 디자인 추가

--- presentation/src/main/kotlin/com/sooum/where_android/view/main/myMeetDetail/modal/schedule/TimeModal.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/main/myMeetDetail/modal/schedule/TimeModal.kt
@@ -1,6 +1,11 @@
 package com.sooum.where_android.view.main.myMeetDetail.modal.schedule
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,7 +15,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
@@ -18,10 +27,13 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MenuDefaults
+import androidx.compose.material3.MenuItemColors
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,20 +41,33 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.PopupProperties
 import com.sooum.where_android.R
 import com.sooum.where_android.theme.Gray500
+import com.sooum.where_android.theme.GrayScale900
 import com.sooum.where_android.theme.pretendard
 import com.sooum.where_android.view.widget.GrayButton
 import com.sooum.where_android.view.widget.ModalHeader
 import com.sooum.where_android.view.widget.PrimaryButton
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlin.math.max
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -234,7 +259,7 @@ private fun <T> TimeRow(
                 color = Color.Black
             )
 
-            Column {
+            Box {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(10.dp)
@@ -262,21 +287,37 @@ private fun <T> TimeRow(
                         tint = Gray500
                     )
                 }
+                val scrollState = rememberScrollState()
+
+                val maxHeight = 220.dp
+
                 DropdownMenu(
                     modifier = Modifier
-                        .wrapContentSize(),
+                        .width(120.dp)
+                        .heightIn(max = maxHeight)
+                        .verticalScrollbar(scrollState),
                     expanded = isDropDownMenuExpanded,
-                    onDismissRequest = { isDropDownMenuExpanded = false }
+                    onDismissRequest = { isDropDownMenuExpanded = false },
+                    containerColor = Color.White,
+                    scrollState = scrollState
                 ) {
+
                     dataList.forEachIndexed { index, data ->
                         DropdownMenuItem(
                             text = {
-                                Text(text = dataTitleList[index])
+                                Text(
+                                    text = dataTitleList[index],
+                                    color = GrayScale900,
+                                    fontWeight = FontWeight.Medium,
+                                    fontSize = 16.sp,
+                                    fontFamily = pretendard
+                                )
                             },
                             onClick = {
                                 isDropDownMenuExpanded = false
                                 onClick(data)
-                            }
+                            },
+                            modifier = Modifier.height(43.dp)
                         )
                     }
                 }
@@ -293,4 +334,31 @@ private fun TimeViewPreview() {
         onDismiss = {},
         onClick = {}
     )
+}
+
+
+fun Modifier.verticalScrollbar(
+    scrollState: ScrollState,
+    scrollBarWidth: Dp = 4.dp,
+    minScrollBarHeight: Dp = 5.dp,
+    scrollBarColor: Color = Color.Gray,
+    cornerRadius: Dp = 2.dp
+): Modifier = composed {
+    drawWithContent {
+        drawContent()
+
+        val visibleHeight: Float = this.size.height - scrollState.maxValue
+        val scrollBarHeight: Float =
+            max(visibleHeight * (visibleHeight / this.size.height), minScrollBarHeight.toPx())
+        val scrollPercent: Float = scrollState.value.toFloat() / scrollState.maxValue
+        val scrollBarOffsetY: Float =
+            scrollState.value + (visibleHeight - scrollBarHeight) * scrollPercent
+
+        drawRoundRect(
+            color = scrollBarColor,
+            topLeft = Offset(this.size.width - scrollBarWidth.toPx(), scrollBarOffsetY),
+            size = Size(scrollBarWidth.toPx(), scrollBarHeight),
+            cornerRadius = CornerRadius(cornerRadius.toPx())
+        )
+    }
 }
