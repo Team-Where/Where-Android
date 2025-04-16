2473ed759d033de975be3e57dd33b0793c41fc8d
jmseb3 <jmseb3@naver.com>
2025-04-16 22:03:18 +0900

    Revert "fix: SplashActivity가 전체화면으로 실행되도록 수정"
    This reverts commit b5875ba8a306d3c96edd802528c4166e409865e7.

--- presentation/src/main/kotlin/com/sooum/where_android/view/splash/SplashActivity.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/splash/SplashActivity.kt
@@ -2,13 +2,10 @@ package com.sooum.where_android.view.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.sooum.where_android.databinding.ActivitySplashBinding
 import com.sooum.where_android.nextActivity
@@ -29,16 +26,6 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         intent.getInviteData()
         setContentView(binding.root)
-
-        //전체화면으로 실행
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.let { windowInsetsController ->
-                windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
-            }
-        } else {
-            window.decorView.systemUiVisibility =
-                (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
-        }
 
         lifecycleScope.launch {
             splashViewModel.checkSplash { dest ->
