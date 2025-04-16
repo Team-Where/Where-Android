b5875ba8a306d3c96edd802528c4166e409865e7
jmseb3 <jmseb3@naver.com>
2025-04-16 11:37:02 +0900

    fix: SplashActivity가 전체화면으로 실행되도록 수정

--- presentation/src/main/kotlin/com/sooum/where_android/view/splash/SplashActivity.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/splash/SplashActivity.kt
@@ -2,21 +2,19 @@ package com.sooum.where_android.view.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.sooum.where_android.databinding.ActivitySplashBinding
 import com.sooum.where_android.nextActivity
 import com.sooum.where_android.view.getInviteData
-import com.sooum.where_android.view.main.MainActivity
-import com.sooum.where_android.view.onboarding.OnBoardingActivity
 import com.sooum.where_android.viewmodel.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
@@ -31,6 +29,16 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         intent.getInviteData()
         setContentView(binding.root)
+
+        //전체화면으로 실행
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.let { windowInsetsController ->
+                windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
+            }
+        } else {
+            window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+        }
 
         lifecycleScope.launch {
             splashViewModel.checkSplash { dest ->
