abc63ea9af386eafaa60e37919210428593e1808
jmseb3 <jmseb3@naver.com>
2025-04-17 00:05:29 +0900

    refactor: Auth 부분 코드 정리

--- presentation/src/main/kotlin/com/sooum/where_android/view/auth/SignInFragment.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/auth/SignInFragment.kt
@@ -1,39 +1,35 @@
 package com.sooum.where_android.view.auth
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
-import com.sooum.domain.model.ApiResult
 import com.sooum.where_android.databinding.FragmentSignInBinding
-import com.sooum.where_android.viewmodel.AuthViewModel
-import kotlinx.coroutines.launch
+import com.sooum.where_android.view.auth.signup.AuthBaseFragment
 
-class SignInFragment : Fragment() {
-    lateinit var binding : FragmentSignInBinding
-    private val viewModel: AuthViewModel by activityViewModels()
+class SignInFragment : AuthBaseFragment() {
+    lateinit var binding: FragmentSignInBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSignInBinding.inflate(inflater, container, false)
-
-        binding.btnLogin.setOnClickListener {
-            viewModel.login(
-                binding.editTextEmail.text.toString(),
-                binding.editTextPassword.text.toString()
-            )
-        }
-
         return binding.root
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            btnLogin.setOnClickListener {
+                viewModel.login(
+                    binding.editTextEmail.text.toString(),
+                    binding.editTextPassword.text.toString()
+                )
+            }
+            imageBack.setOnClickListener {
+                popBackStack()
+            }
+        }
+    }
 }
--- presentation/src/main/kotlin/com/sooum/where_android/view/auth/signup/AuthBaseFragment.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/auth/signup/AuthBaseFragment.kt
@@ -1,22 +1,17 @@
 package com.sooum.where_android.view.auth.signup
 
-import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.viewbinding.ViewBinding
+import com.sooum.where_android.showSimpleToast
 import com.sooum.where_android.view.auth.AuthActivity
 import com.sooum.where_android.viewmodel.AuthViewModel
 
 abstract class AuthBaseFragment : Fragment() {
+    protected val viewModel: AuthViewModel by activityViewModels()
 
-    protected fun showToast(message: String) {
-        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
-    }
+    protected fun showToast(message: String) = showSimpleToast(message)
+
 
     protected fun navigateTo(fragment: Fragment) {
         (activity as? AuthActivity)?.navigateToFragment(fragment)
