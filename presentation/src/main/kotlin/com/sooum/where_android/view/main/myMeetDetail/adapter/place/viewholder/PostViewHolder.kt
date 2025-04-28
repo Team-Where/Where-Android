beef08b84374243d150c81d39a2a6e776e5c8463
Wonddak <jmseb3@naver.com>
2025-04-28 17:37:24 +0900

    feat: 장소 아이템 UI 로직 분리 (main)
    장소 아이템의 UI 로직을 `initDefaultPlace` 함수로 분리하여
    `SelectedPlaceListAdapter`와 `PostViewHolder`에서 재사용합니다.
    
    이를 통해 코드 중복을 줄이고 유지보수성을 향상시킵니다.

--- presentation/src/main/kotlin/com/sooum/where_android/view/main/myMeetDetail/adapter/place/viewholder/PostViewHolder.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/main/myMeetDetail/adapter/place/viewholder/PostViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.sooum.domain.model.PlaceItem
 import com.sooum.where_android.databinding.ItemUnselectedPlaceBinding
+import com.sooum.where_android.databinding.LayoutPlaceDefaultBinding
 import com.sooum.where_android.view.main.myMeetDetail.adapter.place.callback.PlaceClickCallBack
 import com.sooum.where_android.view.main.myMeetDetail.adapter.place.callback.startKakaoMapUri
 import com.sooum.where_android.view.main.myMeetDetail.adapter.place.callback.startNaverMapUri
@@ -19,30 +20,7 @@ class PostViewHolder(
     fun bind(placeItem: PlaceItem) {
         val place = placeItem.place
         with(binding) {
-            updateLikeUI(place.myLike, place.likeCount)
-            with(contentArea) {
-                textPlaceName.text = place.name
-                textAddress.text = place.address
-
-                heartArea.setOnClickListener {
-                    placeClickCallBack?.likeChange(place.id)
-                }
-
-                btnNaverMap.setOnClickListener {
-                    placeClickCallBack?.startNaverMapUri(place.naverLink)
-                }
-
-                btnKakaoMap.setOnClickListener {
-                    placeClickCallBack?.startKakaoMapUri(place.kakaoLink)
-                }
-
-                root.setOnClickListener {
-                    placeClickCallBack?.clickPlace(place.id)
-                }
-
-                textCommentNumber.text = placeItem.commentCount.toString()
-            }
-
+            contentArea.initDefaultPlace(placeItem, placeClickCallBack)
             if (place.together) {
                 textTogether.visibility = View.VISIBLE
             } else {
@@ -51,12 +29,41 @@ class PostViewHolder(
         }
     }
 
-    private fun updateLikeUI(isLiked: Boolean, likeCount: Int) {
-        with(binding.contentArea) {
-            textLikeNumber.isSelected = isLiked
-            textLike.isSelected = isLiked
-            iconHeart.isSelected = isLiked
-            textLikeNumber.text = if (likeCount > 0) likeCount.toString() else ""
-        }
+}
+
+/**
+ * LayoutPlaceDefaultBinding에 대한 공통 연결자 확장 함수
+ */
+fun LayoutPlaceDefaultBinding.initDefaultPlace(
+    placeItem: PlaceItem,
+    placeClickCallBack: PlaceClickCallBack?
+) {
+    val place = placeItem.place
+    textPlaceName.text = place.name
+    textAddress.text = place.address
+    with(place.myLike) {
+        textLikeNumber.isSelected = this
+        textLike.isSelected = this
+        iconHeart.isSelected = this
     }
+
+    textLikeNumber.text = place.likeCount.toString()
+
+    heartArea.setOnClickListener {
+        placeClickCallBack?.likeChange(place.id)
+    }
+
+    btnNaverMap.setOnClickListener {
+        placeClickCallBack?.startNaverMapUri(place.naverLink)
+    }
+
+    btnKakaoMap.setOnClickListener {
+        placeClickCallBack?.startKakaoMapUri(place.kakaoLink)
+    }
+
+    root.setOnClickListener {
+        placeClickCallBack?.clickPlace(place.id)
+    }
+
+    textCommentNumber.text = placeItem.commentCount.toString()
 }
