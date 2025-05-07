17a51e39ef5f8f06053aa6211ab8d465aff97563
park-hwaan <parkhwan1129@gmail.com>
2025-05-07 14:33:51 +0900

    feat: 온보딩 WormDotsIndicator 적용

--- presentation/src/main/kotlin/com/sooum/where_android/viewmodel/SplashViewModel.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/viewmodel/SplashViewModel.kt
@@ -50,7 +50,7 @@ class SplashViewModel @Inject constructor(
                     OnBoardingActivity::class.java
                 } else {
                     //로그인 화면으로...
-                    AuthActivity::class.java
+                    OnBoardingActivity::class.java
                 }
             }
             complete(dest)
