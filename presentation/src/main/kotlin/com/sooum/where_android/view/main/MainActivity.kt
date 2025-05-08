bba3e6eabadcdf5684b0ba4ea047befe94172bad
Wonddak <jmseb3@naver.com>
2025-05-08 12:16:25 +0900

    feat: 로컬 알람 클릭 시 모임 상세 페이지로 이동
    - 로컬 알람을 클릭했을 때, 해당 모임의 상세 페이지로 이동하도록 기능을 추가했습니다.
    - `LocalAlarmProvider`를 통해 알람 정보를 전달받아 `MyMeetActivity`를 실행합니다.
    - `onNewIntent` 에서 `getLocalAlarmProvider`를 통해 알람정보가 있으면 상세 페이지로 이동합니다.
    - `startToDetail` 함수를 통해 상세 페이지로 이동하는 로직을 분리했습니다.

--- presentation/src/main/kotlin/com/sooum/where_android/view/main/MainActivity.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/main/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import com.sooum.where_android.databinding.ActivityMainBinding
+import com.sooum.where_android.view.LocalAlarmProvider
 import com.sooum.where_android.view.checkInviteData
 import com.sooum.where_android.view.getLocalAlarmProvider
 import com.sooum.where_android.view.main.myMeetDetail.MyMeetActivity
@@ -29,6 +30,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         intent?.checkInviteData(this@MainActivity)
+        intent?.getLocalAlarmProvider()?.let {
+            it.startToDetail()
+        }
 
         with(binding.composeView) {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnDetachedFromWindowOrReleasedFromPool)
@@ -49,14 +53,18 @@ class MainActivity : AppCompatActivity() {
         super.onNewIntent(intent)
         intent?.checkInviteData(this@MainActivity)
         intent?.getLocalAlarmProvider()?.let {
-            val id = it.meetId
-            startActivity(
-                Intent(this, MyMeetActivity::class.java).apply {
-                    putExtras(Bundle().apply {
-                        putInt(MyMeetActivity.MEET_ID, id)
-                    })
-                }
-            )
+            it.startToDetail()
         }
+    }
+
+    private fun LocalAlarmProvider.startToDetail() {
+        val id = meetId
+        startActivity(
+            Intent(this@MainActivity, MyMeetActivity::class.java).apply {
+                putExtras(Bundle().apply {
+                    putInt(MyMeetActivity.MEET_ID, id)
+                })
+            }
+        )
     }
 }
