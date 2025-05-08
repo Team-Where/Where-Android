bbec5664bf58e19276893e931da6bea0871e0aa1
Wonddak <jmseb3@naver.com>
2025-05-08 10:44:51 +0900

    feat: MainActivity에서 새로운 인텐트 처리 로직 추가
    - `onNewIntent`에서 인텐트를 받아 초대 데이터 또는 로컬 알람 데이터를 확인하고 처리하도록 수정했습니다.
    - 로컬 알람 데이터를 받으면 `MyMeetActivity`를 시작하도록 로직을 추가했습니다.
    - `MyMeetActivity` 시작 시 `MEET_ID`를 `Bundle`에 넣어 전달하도록 추가했습니다.

--- presentation/src/main/kotlin/com/sooum/where_android/view/main/MainActivity.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/main/MainActivity.kt
@@ -2,22 +2,17 @@ package com.sooum.where_android.view.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import com.sooum.data.datastore.AppManageDataStore
 import com.sooum.where_android.databinding.ActivityMainBinding
 import com.sooum.where_android.view.checkInviteData
+import com.sooum.where_android.view.getLocalAlarmProvider
+import com.sooum.where_android.view.main.myMeetDetail.MyMeetActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
 val LocalActivity = compositionLocalOf<MainActivity> {
@@ -47,14 +42,21 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
-
-
         setContentView(binding.root)
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         intent?.checkInviteData(this@MainActivity)
+        intent?.getLocalAlarmProvider()?.let {
+            val id = it.meetId
+            startActivity(
+                Intent(this, MyMeetActivity::class.java).apply {
+                    putExtras(Bundle().apply {
+                        putInt(MyMeetActivity.MEET_ID, id)
+                    })
+                }
+            )
+        }
     }
 }
