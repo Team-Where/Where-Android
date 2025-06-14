12aa262ba09b2fa32a89aa6ebdb914bf0271ebe1
jmseb3 <jmseb3@naver.com>
2025-06-14 17:37:23 +0900

    refactor: `HamburgerBaseFragment`에서 불필요한 `navigateTo` 함수 제거
    - `HamburgerBaseFragment`에서 사용되지 않는 `navigateTo` 함수를 삭제했습니다.

--- presentation/src/main/kotlin/com/sooum/where_android/view/hamburger/HamburgerBaseFragment.kt
+++ presentation/src/main/kotlin/com/sooum/where_android/view/hamburger/HamburgerBaseFragment.kt
@@ -2,16 +2,11 @@ package com.sooum.where_android.view.hamburger
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
-import com.sooum.where_android.view.auth.AuthActivity
 import com.sooum.where_android.view.common.BaseViewBindingFragment
 
 abstract class HamburgerBaseFragment<VB : ViewBinding>(
     bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
 ) : BaseViewBindingFragment<VB>(bindingInflater) {
 
-    protected fun navigateTo(fragment: Fragment) {
-        (activity as? AuthActivity)?.navigateToFragment(fragment)
-    }
 }
