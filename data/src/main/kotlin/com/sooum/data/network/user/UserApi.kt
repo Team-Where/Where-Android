5fe2227eca974b7486e91818942cfdc7b46eadf0
Wonddak <jmseb3@naver.com>
2025-06-17 10:01:19 +0900

    feat: 유저 정보 API 연동 (myPage)
    - 마이페이지 정보 조회 API 연동
    - 프로필 이미지 추가/수정/삭제 API 연동
    - 닉네임 수정 API 연동
    - 관련 요청/응답 데이터 클래스 추가

---
+++ data/src/main/kotlin/com/sooum/data/network/user/UserApi.kt
@@ -0,0 +1,50 @@
+package com.sooum.data.network.user
+
+import com.sooum.data.network.user.request.EditNicknameRequest
+import com.sooum.data.network.user.response.AddProfileResponse
+import com.sooum.data.network.user.response.EditProfileResponse
+import okhttp3.MultipartBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Part
+import retrofit2.http.Path
+
+interface UserApi {
+
+    @Headers("Cache-Control: max-age=60")
+    @GET("api/user/myPage/{userId}")
+    suspend fun getMyPage(
+        @Path("userId") userId: Int
+    ): Response<String>
+
+    @Multipart
+    @POST("api/user/{userId}/upload")
+    suspend fun addProfileImage(
+        @Path("userId") userId: Int,
+        @Part imageFile: MultipartBody.Part
+    ): Response<AddProfileResponse>
+
+    @Multipart
+    @PUT("api/user/{userId}/edit")
+    suspend fun editProfileImage(
+        @Path("userId") userId: Int,
+        @Part imageFile: MultipartBody.Part
+    ): Response<EditProfileResponse>
+
+    @DELETE("api/user/{userId}/delete")
+    suspend fun deleteProfileImage(
+        @Path("userId") userId: Int,
+    ): Response<Any>
+
+    @PUT("api/user/{userId}/nickname")
+    suspend fun editNickName(
+        @Path("userId") userId: Int,
+        @Body data: EditNicknameRequest
+    ): Response<Any>
+}
